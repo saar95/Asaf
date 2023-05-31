4687d8262b87dda72345a281a7f394ae8ac7babf
Saar <saar9550@gmail.com>
2023-06-01 02:01:42 +0300

    Fix build gardle + firebase database

--- app/src/main/java/com/example/asaf/LoginActivity.java
+++ app/src/main/java/com/example/asaf/LoginActivity.java
@@ -2,13 +2,43 @@ package com.example.asaf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//firebase
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance("https://asaf-b2993-default-rtdb.europe-west1.firebasedatabase.app/");
+        myRef=database.getReference();
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    public void registerOnClick(View view) {
+        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+    }
+
 }
