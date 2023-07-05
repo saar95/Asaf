13be0a756ab9790294fd4f43f5c179aba590caf2
uriyashemla <uriyashem1994@gmail.com>
2023-07-05 23:34:39 +0300

    adding profile activity

--- app/src/main/java/com/example/asaf/ProfileActivity.java
+++ app/src/main/java/com/example/asaf/ProfileActivity.java
@@ -1,14 +1,47 @@
 package com.example.asaf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private HeaderHandler headerHandler;
+    private FooterHandler footerHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        setupHeader();
+        setupFooter();
+    }
+
+    private void setupFooter() {
+        // Find the buttons in the footer layout
+        AppCompatButton mySuggestionsButton = findViewById(R.id.btn_my_suggestions);
+        AppCompatButton myRidesButton = findViewById(R.id.btn_my_rides);
+        AppCompatButton myChatsButton = findViewById(R.id.btn_my_chats);
+
+        // Create an instance of FooterHandler and set it as the OnClickListener for the buttons
+        footerHandler = new FooterHandler(this);
+        mySuggestionsButton.setOnClickListener(footerHandler);
+        myRidesButton.setOnClickListener(footerHandler);
+        myChatsButton.setOnClickListener(footerHandler);
+    }
+
+    private void setupHeader() {
+        headerHandler = new HeaderHandler(this);
+
+        ImageButton menuButton = findViewById(R.id.btn_open_menu);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                headerHandler.openSideMenu();
+            }
+        });
     }
 }
